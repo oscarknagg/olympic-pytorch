874cb74c226b80e2fcaf2dee531001206921cf87
Oscar Knagg <oscar@knagg.co.uk>
2018-11-30 19:51:11 +0000

    Added some convenience layers from Keras

---
+++ README.md
@@ -0,0 +1,8 @@
+# Olympic PyTorch
+
+Python 3.6 package implementing Keras functionality for PyTorch.
+
+Includes
+- fit(). A functional replacement for Keras's model.fit()
+- Most Callbacks from Keras have been ported
+- Some convenience layers such as Flatten and GlobalPooling
---
+++ olympic/layers.py
@@ -0,0 +1,53 @@
+from torch import nn
+import torch.nn.functional as F
+
+
+class Flatten(nn.Module):
+    """Converts N-dimensional Tensor of shape [batch_size, d1, d2, ..., dn] to 2-dimensional Tensor
+    of shape [batch_size, d1*d2*...*dn].
+
+    # Arguments
+        input: Input tensor
+    """
+    def forward(self, input):
+        return input.view(input.size(0), -1)
+
+
+class GlobalMaxPool1d(nn.Module):
+    """Performs global max pooling over the entire length of a batched 1D tensor
+
+    # Arguments
+        input: Input tensor
+    """
+    def forward(self, input):
+        return F.max_pool1d(input, kernel_size=input.size()[2:]).view(-1, input.size(1))
+
+
+class GlobalAvgPool1d(nn.Module):
+    """Performs global average pooling over the entire length of a batched 1D tensor
+
+        # Arguments
+            input: Input tensor
+        """
+    def forward(self, *input):
+        return F.avg_pool1d(input, kernel_size=input.size()[2:]).view(-1, input.size(1))
+
+
+class GlobalMaxPool2d(nn.Module):
+    """Performs global max pooling over the entire height and width of a batched 2D tensor
+
+    # Arguments
+        input: Input tensor
+    """
+    def forward(self, input):
+        return F.max_pool2d(input, kernel_size=input.size()[2:]).view(-1, input.size(1))
+
+
+class GlobalAvgPool2d(nn.Module):
+    """Performs global average pooling over the entire height and width of a batched 2D tensor
+
+    # Arguments
+        input: Input tensor
+    """
+    def forward(self, input):
+        return F.avg_pool2d(input, kernel_size=input.size()[2:]).view(-1, input.size(1))
