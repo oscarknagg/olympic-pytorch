3a8976de8f4753649f56c3f52af30243283faa86
Oscar Knagg <oscar@knagg.co.uk>
2018-11-30 20:01:38 +0000

    Added setup.py file

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(name='olympic',
+      version='0.1',
+      description='A port of the Keras API to PyTorch',
+      url='http://github.com/oscarknagg/olympic-pytorch',
+      author='Oscar Knagg',
+      author_email='oscar@knagg.co.uk',
+      license='MIT',
+      packages=['olympic'],
+      zip_safe=False)
